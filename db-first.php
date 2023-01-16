0981edebd179604d7edeb3770ec330ffe4b847c1
Yari <narducciyarigmail.com>
2023-01-16 16:00:23 +0100

    Modifica tabella ed aggiunta screen

--- db-first.php
+++ db-first.php
@@ -10,14 +10,14 @@
 
 /* `macchine usate`.`db-first` */
 $db_first = array(
-  array('id' => '1','Marca & Modello' => 'Peugeot 208
-PureTech 75 S&S Active Pack','Prezzo' => '17356','Immatricolazione' => '2020-01-16','Cambio' => 'Manuale'),
-  array('id' => '2','Marca & Modello' => 'Toyota Yaris
-1.5 Hybrid Business','Prezzo' => '16000','Immatricolazione' => '2021-06-16','Cambio' => 'Automatico'),
-  array('id' => '3','Marca & Modello' => 'Jeep Compass
-1.4 MultiAir Limited','Prezzo' => '22654','Immatricolazione' => '2021-05-26','Cambio' => 'Manuale'),
-  array('id' => '4','Marca & Modello' => 'Toyota RAV 4
-2.5 HV 218CV E-CVT Dynamic','Prezzo' => '30000','Immatricolazione' => '2022-03-10','Cambio' => 'Automatico'),
-  array('id' => '5','Marca & Modello' => 'Renault Clio
-TCe 90CV Duel','Prezzo' => '13000','Immatricolazione' => '2020-05-15','Cambio' => 'Manuale')
+  array('id' => '1','Marca' => 'Peugeot 208
+','Modello' => 'PureTech 75 S&S Active Pack','Prezzo' => '17356','Immatricolazione' => '2020-01-16','Cambio' => 'Manuale'),
+  array('id' => '2','Marca' => 'Toyota Yaris
+','Modello' => '1.5 Hybrid Business','Prezzo' => '16000','Immatricolazione' => '2021-06-16','Cambio' => 'Automatico'),
+  array('id' => '3','Marca' => 'Jeep Compass
+','Modello' => '1.4 MultiAir Limited','Prezzo' => '22654','Immatricolazione' => '2021-05-26','Cambio' => 'Manuale'),
+  array('id' => '4','Marca' => 'Toyota RAV 4
+','Modello' => '2.5 HV 218CV E-CVT Dynamic','Prezzo' => '30000','Immatricolazione' => '2022-03-10','Cambio' => 'Automatico'),
+  array('id' => '5','Marca' => 'Renault Clio
+','Modello' => 'TCe 90CV Duel','Prezzo' => '13000','Immatricolazione' => '2020-05-15','Cambio' => 'Manuale')
 );
