f4b28a51bed7d680ccd59c9ac9baa2f9b27b2ed6
Yari <narducciyarigmail.com>
2023-01-16 14:28:07 +0100

    screen con conversione tabella in file json e php

---
+++ db-first.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.2
+ */
+
+/**
+ * Database `macchine usate`
+ */
+
+/* `macchine usate`.`db-first` */
+$db_first = array(
+  array('id' => '1','Marca & Modello' => 'Peugeot 208
+PureTech 75 S&S Active Pack','Prezzo' => '17356','Immatricolazione' => '2020-01-16','Cambio' => 'Manuale'),
+  array('id' => '2','Marca & Modello' => 'Toyota Yaris
+1.5 Hybrid Business','Prezzo' => '16000','Immatricolazione' => '2021-06-16','Cambio' => 'Automatico'),
+  array('id' => '3','Marca & Modello' => 'Jeep Compass
+1.4 MultiAir Limited','Prezzo' => '22654','Immatricolazione' => '2021-05-26','Cambio' => 'Manuale'),
+  array('id' => '4','Marca & Modello' => 'Toyota RAV 4
+2.5 HV 218CV E-CVT Dynamic','Prezzo' => '30000','Immatricolazione' => '2022-03-10','Cambio' => 'Automatico'),
+  array('id' => '5','Marca & Modello' => 'Renault Clio
+TCe 90CV Duel','Prezzo' => '13000','Immatricolazione' => '2020-05-15','Cambio' => 'Manuale')
+);
